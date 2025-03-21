e2466ad8cf43bb84e832ed0df39bd017315e1ba3
AshunMotley <ashunmotley25@gmail.com>
2025-03-20 22:04:32 -0500

    add first character

--- parse.cpp
+++ parse.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 //this extracrt characteristic part of a number string 
-bool characteristic(const char numString[], int& c) {
+bool characteristic(const char numString[], int& c) 
+{
+    int i = 0;
+    while (numString[i] == ' ') i++; // skip first spaces
+
+    int sign = 1;
+    if(numString[i] == '-' || numString[i] == '+') 
+    {
+        if(numString[i] == '-') sign = -1;
+        i++;
+    }
+    //make sure the first character is a digit
     
 }
