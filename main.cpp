b06a489a5d8ab9b1aac505d9b9fa18f686ab5f5a
AshunMotley <ashunmotley25@gmail.com>
2025-03-21 14:49:15 -0500

    added in the function to my main from parse.cpp(test area)

--- main.cpp
+++ main.cpp
@@ -72,11 +72,28 @@ int main()
 
     return 0;
 } 
-//--
-bool characteristic(const char numString[], int& c)
+// Added in the characteristc function from my parse.cpp // test area
+bool characteristic(const char numString[], int& c) 
 {
-    //hard coded return value to make the main() work
-    c = 123;
+    int i = 0;
+    while (numString[i] == ' ') i++; // skip first spaces if any
+
+    int sign = 1;
+    if(numString[i] == '-' || numString[i] == '+') 
+    {
+        if(numString[i] == '-') sign = -1;
+        i++;
+    }
+    //make sure the first character is a digit
+    if (numString[i] < '0' || numString[i] > '9') return false;
+
+    c = 0;
+    while (numString[i] >= '0' && numString[i] <= '9')
+    {
+        c = c * 10 + (numString[i] - '0');
+        i++;
+    }
+    c *= sign;
     return true;
 }
 //--
