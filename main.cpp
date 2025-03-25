b48a8096f23a217f769e4b684ae546d121d41603
AlejandroT23 <atoledo1@carthage.edu>
2025-03-25 16:44:48 -0500

    Added arithmeticOperation()
    The code for each of the operations was copied and pasted, only changed for specific instances, so I felt like it lumping it into one function was better, with the code being based on operator sign.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 using namespace std;
 
@@ -7,22 +6,24 @@ bool characteristic(const char numString[], int& c);
 bool mantissa(const char numString[], int& numerator, int& denominator);
 
 bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
-bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len); 
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 
-int improperFraction(int& num, int& den);
-void commonDenom(int& c1, int& n1, int& d1, int& c2, int& n2, int& d2);
+int improperFraction(int& charNum, int& num, int& den);
+void commonDenom(int& n1, int& d1, int& n2, int& d2, int& commonDen);
 void getResult(int& num, int& den, char* result, int& len);
 void convertIntToChar(int& num, int& index, char* resultArr, int& len);
 void inverseFraction (int& num, int& den);
+
+bool arithmeticOperation (int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len, char symbol);
 
 int main()
 {
     //this c-string, or array of 8 characters, ends with the null terminating character '\0'
     //['1', '2', '3', '.', '4', '5', '6', '\0']
-    const char number[] = "123.456"; 
+    const char number[] = "123.456";
     int c, n, d;
 
     //if both conversions from c-string to integers can take place
@@ -45,13 +46,13 @@ int main()
     int c2, n2, d2;
 
     //initialize the values
-    c1 = 1;
-    n1 = 1;
-    d1 = 2;
+    c1 = 23;
+    n1 = 3;
+    d1 = 5;
 
     c2 = 2;
-    n2 = 2;
-    d2 = 3; 
+    n2 = 7;
+    d2 = 4;
 
     //if the c-string can hold at least the characteristic
     if(add(c1, n1, d1, c2, n2, d2, answer, 10))
@@ -65,6 +66,28 @@ int main()
         cout<<"Error on add"<<endl;
     }
 
+    if(subtract(c1, n1, d1, c2, n2, d2, answer, 10))
+    {
+        //display string with answer 4.1666666 (cout stops printing at the null terminating character)
+        cout<<"Answer: "<<answer<<endl;
+    }
+    else
+    {
+        //display error message
+        cout<<"Error on subtract"<<endl;
+    }
+    
+    if(multiply(c1, n1, d1, c2, n2, d2, answer, 10))
+    {
+        //display string with answer 4.1666666 (cout stops printing at the null terminating character)
+        cout<<"Answer: "<<answer<<endl;
+    }
+    else
+    {
+        //display error message
+        cout<<"Error on multiply"<<endl;
+    }
+    
     if(divide(c1, n1, d1, c2, n2, d2, answer, 10))
     {
         //display string with answer
@@ -77,7 +100,7 @@ int main()
     }
 
     return 0;
-} 
+}
 //--
 bool characteristic(const char numString[], int& c)
 {
@@ -96,62 +119,22 @@ bool mantissa(const char numString[], int& numerator, int& denominator)
 //--
 bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
-    int n1_improper = improperFraction(c1, n1, d1);
-    int n2_improper = improperFraction(c2, n2, d2);
-    
-    int commonDen = 0;
-    
-    commonDenom(n1_improper, d1, n2_improper, d2, commonDen);
-    
-    int n3 = n1_improper + n2_improper;
-    
-    getResult(n3, commonDen, result, len);
-    
-    return true;
+    return arithmeticOperation(c1, n1, d1, c2, n2, d2, result, len, '+');
 }
 //--
 bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
-    int n1_improper = improperFraction(c1, n1, d1);
-    int n2_improper = improperFraction(c2, n2, d2);
-    
-    int commonDen = 0;
-    
-    commonDenom(n1_improper, d1, n2_improper, d2, commonDen);
-    
-    int n3 = n1_improper - n2_improper;
-    
-    getResult(n3, commonDen, result, len);
-    
-    return true;
+    return arithmeticOperation(c1, n1, d1, c2, n2, d2, result, len, '-');
 }
 //--
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
-    int n1_improper = improperFraction(c1, n1, d1);
-    int n2_improper = improperFraction(c2, n2, d2);
-    
-    int n3 = n1_improper * n2_improper;
-    int d3 = d1 * d2;
-    
-    getResult(n3, d3, result, len);
-    
-    return true;
+    return arithmeticOperation(c1, n1, d1, c2, n2, d2, result, len, '*');
 }
 //--
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
-    int n1_improper = improperFraction(c1, n1, d1);
-    int n2_improper = improperFraction(c2, n2, d2);
-    
-    inverseFraction(n2_improper, d2);
-    
-    int n3 = n1_improper * n2_improper;
-    int d3 = d1 * d2;
-    
-    getResult(n3, d3, result, len);
-    
-    return true;
+    return arithmeticOperation(c1, n1, d1, c2, n2, d2, result, len, '/');
 }
 //--
 int improperFraction(int& charNum, int& num, int& den)
@@ -159,7 +142,7 @@ int improperFraction(int& charNum, int& num, int& den)
     return num = (charNum * den) + num;
 }
 //--
-void commonDenom(int& n1, int& d1, int& n2, int& d2)
+void commonDenom(int& n1, int& d1, int& n2, int& d2, int& commonDen)
 {
     commonDen = d1 * d2;
     
@@ -179,7 +162,6 @@ void getResult(int& num, int& den, char* result, int& len)
     result[index] = '.';
     index++;
     
-    cout << den << endl;
     int remain = num % den;
     //int remainCopy = remain;
     
@@ -191,8 +173,6 @@ void getResult(int& num, int& den, char* result, int& len)
             charNum = (remain * 10) / den;
             remain = (remain * 10) % den;
             convertIntToChar(charNum, index, result, len);
-            
-            cout << remain << endl;
         }
     }
     
@@ -237,4 +217,59 @@ void inverseFraction (int& num, int& den)
     int num2 = num;
     num = den;
     den = num2;
+}
+
+bool arithmeticOperation (int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len, char symbol)
+{
+    int n1_improper = improperFraction(c1, n1, d1);
+    int n2_improper = improperFraction(c2, n2, d2);
+    
+    bool retVal = false;
+    
+    if (symbol == '+')
+    {
+        int commonDen = 0;
+        commonDenom(n1_improper, d1, n2_improper, d2, commonDen);
+        
+        int n3 = n1_improper + n2_improper;
+        getResult(n3, commonDen, result, len);
+        
+        retVal = true;
+    }
+    else if (symbol == '-')
+    {
+        int commonDen = 0;
+        commonDenom(n1_improper, d1, n2_improper, d2, commonDen);
+        
+        int n3 = n1_improper - n2_improper;
+        getResult(n3, commonDen, result, len);
+        
+        retVal = true;
+    }
+    else if (symbol == '*')
+    {
+        int n3 = n1_improper * n2_improper;
+        int d3 = d1 * d2;
+        
+        getResult(n3, d3, result, len);
+        
+        retVal = true;
+    }
+    else if (symbol == '/')
+    {
+        inverseFraction(n2_improper, d2);
+        
+        int n3 = n1_improper * n2_improper;
+        int d3 = d1 * d2;
+        
+        getResult(n3, d3, result, len);
+        
+        retVal = true;
+    }
+    else
+    {
+        retVal = false;
+    }
+    
+    return retVal;
 }
